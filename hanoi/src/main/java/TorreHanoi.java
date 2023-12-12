6ab0327651e375a455d769c114296d050614c366
Felipe Silva Corrêa <lipesc41@gmail.com>
2023-12-12 13:56:41 -0300

    .

--- hanoi/src/main/java/TorreHanoi.java
+++ hanoi/src/main/java/TorreHanoi.java
@@ -4,11 +4,8 @@ public class TorreHanoi {
 
     public void hanoi(int n, char origem, char destino, char auxiliar) {
         if (n > 0) {
-            // Mova n-1 discos de origem para auxiliar
             hanoi(n - 1, origem, auxiliar, destino);
-            // Mova o disco restante de origem para destino
             movimentos++;
-            // Mova os n-1 discos de auxiliar para destino
             hanoi(n - 1, auxiliar, destino, origem);
         }
     }
